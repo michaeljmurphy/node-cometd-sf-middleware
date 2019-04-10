c1bbf3a662d62826f950eb0b4687254852df9eb3
michael murphy <michaelmurphy@salesforce.com>
2019-04-10 13:52:55 +0100

    handle multiple connections#

--- node-cometd-middleware.js
+++ node-cometd-middleware.js
@@ -1,68 +1,101 @@
+/*
+  on startup
+  - create server
+  on subscribe message from client
+  - create a channel (local)
+  - subscribe client to channel
+  - get session token from map
+  - try to sub to channel (sf)
+  - broker messages between sf & local channel
+  */
+
 const jsforce = require("jsforce");
 const http = require("http");
-const async = require("async");
 const cometd = require('cometd-nodejs-server');
 
-
 const loginUrl = process.env.SF_URI;
-const sessionToken = process.env.SF_TOKEN;
-const channel = process.env.SF_CHANNEL;
-
-const conn = new jsforce.Connection({
-    instanceUrl : loginUrl,
-    accessToken : sessionToken
-});
-
-
+const tokenCredentialMap = process.env.SF_CRED_MAP;
 
 /* Create a cometd server and expose it */
 const cometdServer = cometd.createCometDServer({
     loglevel : 'debug'
 });
+
+const metaSubscribe = cometdServer.getServerChannel('/meta/subscribe');
 const httpServer = http.createServer(cometdServer.handle);
+
+
 httpServer.listen(9000, function() {
     var port = httpServer.address().port;
-    console.log('listening on localhost:' + port);
     _uri = 'http://localhost:' + port + '/cometd';
 });
 
+
+
+function getSubscribeRequests() {
+    metaSubscribe.addListener('message', function(session, channel, message, callback) {
+        var subscription = message.subscription;
+        var request = cometdServer.context.request;
+        var sfCredentials = '';
+        var credMap = JSON.parse(tokenCredentialMap);
+        
+        if(request) {
+            var sessionToken = request.headers['authorization'];
+            sfCredentials = credMap[sessionToken];
+        }
+
+        var sfAccessToken = getSfSessionToken(sfCredentials);
+        var localChannel = createServerChannel(subscription);
+        doPubSub(sfAccessToken, localChannel);
+
+        // Invoke the callback to signal that handling is complete.
+        callback();
+    });
+}
+
+
 /* Create a channel with the same name as the
    channel we created on SF */
-const cometDChannel = cometdServer.createServerChannel(channel);
-console.dir(cometDChannel);
-console.dir(cometDChannel.publish);
+function createServerChannel(channel) {
+    return cometdServer.createServerChannel(channel);
+}
 
 
-/* In parallel we need to create a session
-/* with SF and listen to messages that are
-/* published to the cometd server we created
-/* so that we can check it is working.  
-/* This will not be necessary outside
-/* of testing purposes */
-async.parallel([
-    /* This function subscribes to the Salesforce EMP
-    /* and publishes messages to the local cometd server 
-    /* that we created earlier */
+/* This function subscribes to the Salesforce EMP
+   /* and publishes messages to the local cometd server 
+   /* that we created earlier */
 
-    function doPubSub() {
-        console.log('doSubscribe(): ' + channel);
-        conn.streaming.topic(channel).subscribe(function(message) {
-            console.log('*** SF Listener');
-            console.dir(message);
-            cometDChannel.publish(null, message);
-        });
-    },
-    
-    function listenToChannel() {
-        console.log('listenToChannel()');
+function doPubSub(sfAccessToken, cometDChannel) {
+    var conn = new jsforce.Connection({
+        instanceUrl : loginUrl,
+        accessToken : sfAccessToken
+    });
 
-        cometDChannel.addListener('message', function(session, channel, message, callback) {
-            // Your message handling here.
-            console.log('*** CometD Listener');
-            console.dir(message);
 
-            // Invoke the callback to signal that handling is complete.
-            callback();
-        });
-    }
-]);
+    conn.streaming.topic(cometDChannel.name).subscribe(function(message) {
+        cometDChannel.publish(null, message);
+    });
+}
+
+
+// get the token for the user from the map
+// create a new connection to the sf endpoint
+// return the instnace of the connection
+function getSfSessionToken(sfCredentials) {
+    var accessToken = '';
+
+    var conn = new jsforce.Connection({
+        loginUrl : loginUrl
+    });
+
+    conn.login(sfCredentials.uid, sfCredentials.pwd, function(err, userInfo) {
+        accessToken = conn.accessToken
+    });
+
+    return accessToken;
+}
+
+
+getSubscribeRequests();
+
+
